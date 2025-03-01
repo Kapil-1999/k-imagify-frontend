a15d23b1579c2bbaed887c664cef2c675ff696d4
Skylabs <code@skylabstech.com>
2025-03-01 18:25:52 +0530

    login api

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -2,14 +2,20 @@ import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
 import { motion } from "framer-motion";
+import axios from "axios";
 
 const Login = () => {
   const [state, setState] = useState("Login");
+  const [userData, setUserData] = useState({
+    name : '',
+    email: "",
+    password: "",
+  });
   const context = useContext(AppContext);
   if (!context) {
       return <p>Error: Context not found!</p>;
   }  
-  const { setShowLogin } = context;
+  const { setShowLogin , backendUrl , setToken , setUser  } = context;
 
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -17,13 +23,63 @@ const Login = () => {
       document.body.style.overflow = "auto";
     };
   }, []);
+
+  const handleChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const submitForm = async(e) => {
+    e.preventDefault();
+    console.log(user);
+    if(state === "Login"){
+      loginUser();
+    }else{
+      registerUser();
+    }    
+  }
+  const loginUser = async() => {
+    try {
+
+      const response = await axios.post(`${backendUrl}user/login`, {email: userData.email, password: userData.password});
+      console.log(response.data);
+      if(response.data.success){
+        setToken(response.data.token);
+        setUser(response.data.user);
+        localStorage.setItem("imagify_token", response.data.token);
+        setShowLogin(false);
+      }else{
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const registerUser = async() => {
+    try {
+      const response = await axios.post(`${backendUrl}user/register`, userData);
+      console.log(response.data);
+      if(response.data.success){
+        setToken(response.data.token);
+        localStorage.setItem("imagify_token", response.data.token);
+        setShowLogin(false);
+      }else{
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div
-
       className="fixed z-10 left-0 top-0 right-0 bottom-0 backdrop-blur-sm bg-black/30 flex 
     justify-center items-center"
     >
-      <motion.form 
+      <motion.form onSubmit={submitForm}
       initial={{ opacity: 0.2, y: 50 }}
       transition={{ duration: 0.3 }}
       whileInView={{ opacity: 1, y: 0 }}
@@ -40,6 +96,9 @@ const Login = () => {
               type="text"
               className="outline-none text-sm"
               placeholder="Full Name"
+              name="name"
+              value={userData.name}
+              onChange={handleChange}
               required
             />
           </div>
@@ -50,6 +109,9 @@ const Login = () => {
             type="email"
             className="outline-none text-sm"
             placeholder="Email id"
+            name="email"
+            value={userData.email}
+            onChange={handleChange}
             required
           />
         </div>
@@ -59,6 +121,9 @@ const Login = () => {
             type="password"
             className="outline-none text-sm"
             placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
             required
           />
         </div>
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,10 +4,17 @@ export const AppContext =  createContext(null);
 const AppContextProvider = (props) => {
     const [user, setUser] = useState(null);
     const [showLogin, setShowLogin] = useState(false);
+    const [token , setToken] = useState(localStorage.getItem("imagify_token"));
+    const [credit, setCredit] = useState(false);
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const value = {
         user,
         setUser ,
-        showLogin, setShowLogin
+        showLogin, 
+        setShowLogin ,
+        backendUrl,
+        token, setToken, 
+        credit, setCredit
     }
 
     return (
